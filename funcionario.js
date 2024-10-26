2605fc8f25944ec1e78f7d7b857bc47d1b7fbe9f
senagab <senagab@gmail.com>
2024-10-25 23:38:33 -0400

    feat: encapsulamento + getters e setters

--- funcionario.js
+++ funcionario.js
@@ -14,24 +14,67 @@ function Funcionario(nome, cargo, salario) {
     // this.salario = salario;
     let _salario = salario;
 
-    this.retornaSalario = function () { // getter
-        return `O salário de ${this.nome} é ${_salario};`
+    this.getSalario = function () { // getter
+        // return `O salário de ${this.nome} é ${_salario};`
+        return _salario;
     }
 
-    this.atribuiSalario = function (valor) { // setter
+    this.setSalario = function (valor) { // setter
         if (typeof valor === 'number') {
             _salario = valor;
         }
     }
 
+    this.aumento = function() {
+        const novoSalario = _salario * 1.1;
+        // console.log(novoSalario);
+        _salario = novoSalario;
+    }
+
     Pessoa.call(this, nome);
 }
 
+function Estagiario(nome) {
+    Funcionario.call(this, nome, "Estagiário", 2000);
+
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.07;
+        this.setSalario(novoSalario);
+
+        // console.log(novoSalario);
+        // _salario = novoSalario;
+    }
+}
+
+function Gerente(nome) {
+    Funcionario.call(this, nome, "Gerente", 10000);
+
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.15;
+        this.setSalario(novoSalario);
+
+        // console.log(novoSalario);
+        // _salario = novoSalario;
+    }
+}
+
 const funcionario1 = new Funcionario("Maria", "dev front-end", 5000);
+const funcionario2 = new Estagiario("Joao");
+const funcionario3 = new Gerente("Paula");
 
-funcionario1.dizOi();
-funcionario1.dizCargo();
+// funcionario1.dizOi();
+// funcionario1.dizCargo();
 
-funcionario1.atribuiSalario('mil');
+// funcionario1.setSalario('mil');
 
-console.log(funcionario1.retornaSalario());
+
+// console.log(funcionario1.getSalario());
+
+funcionario1.aumento();
+console.log(funcionario1.getSalario());
+
+funcionario2.aumento();
+console.log(funcionario2.getSalario());
+
+funcionario3.aumento();
+console.log(funcionario3.getSalario());
